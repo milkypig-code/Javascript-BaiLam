788e079bb2296befc520fe9340f23d88fea8fe27
milkypig-code <nguyennamhathanh@gmail.com>
2025-12-28 11:35:42 +0700

    dung isNaN de kiem tra bai 13

--- Bai 11 - Bai tap toan tu/Diem trung binh/main.js
+++ Bai 11 - Bai tap toan tu/Diem trung binh/main.js
@@ -1,19 +1,34 @@
-let diemVan = Number(prompt("Nhập vào điểm văn của sinh viên:"));
-let diemToan = Number(prompt("Nhập vào điểm toán của sinh viên:"));
-let diemAnh = Number(prompt("Nhập vào điểm anh của sinh viên:"));
+// let diemVan = Number(prompt("Nhập vào điểm văn của sinh viên:"));
+// if (diemVan < 0 || diemVan > 10 || isNaN(diemVan)) {
+//   alert("Điểm số phải nằm trong khoảng từ 0 đến 10");
+//   throw new Error("Dữ liệu không hợp lệ!");
+// }
 
-if (
-  diemVan < 0 ||
-  diemVan > 10 ||
-  diemToan < 0 ||
-  diemToan > 10 ||
-  diemAnh < 0 ||
-  diemAnh > 10
-) {
-  alert("Điểm số phải nằm trong khoảng từ 0 đến 10");
-  throw new Error("Dữ liệu không hợp lệ!");
+// let diemToan = Number(prompt("Nhập vào điểm toán của sinh viên:"));
+// if (diemToan < 0 || diemToan > 10 || isNaN(diemToan)) {
+//   alert("Điểm số phải nằm trong khoảng từ 0 đến 10");
+//   throw new Error("Dữ liệu không hợp lệ!");
+// }
+
+// let diemAnh = Number(prompt("Nhập vào điểm anh của sinh viên:"));
+// if (diemAnh < 0 || diemAnh > 10 || isNaN(diemAnh)) {
+//   alert("Điểm số phải nằm trong khoảng từ 0 đến 10");
+//   throw new Error("Dữ liệu không hợp lệ!");
+// }
+
+function nhapDiem(monHoc) {
+  let diem = Number(prompt(`Nhập vào điểm ${monHoc} của sinh viên:`));
+  if (diem < 0 || diem > 10 || isNaN(diem)) {
+    alert("Điểm số phải nằm trong khoảng từ 0 đến 10");
+    throw new Error("Dữ liệu không hợp lệ!");
+  }
+  return diem;
 }
+
+let diemVan = nhapDiem("văn");
+let diemToan = nhapDiem("toán");
+let diemAnh = nhapDiem("anh");
 
 let diemTrungBinh = (diemVan + diemToan + diemAnh) / 3;
 
-alert(`1. Điểm trung bình của sinh viên là: ${diemTrungBinh.toFixed(2)}`);
+alert(`Điểm trung bình của sinh viên là: ${diemTrungBinh.toFixed(2)}`);
